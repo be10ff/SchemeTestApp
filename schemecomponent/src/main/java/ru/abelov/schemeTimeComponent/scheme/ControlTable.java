3702dcac22d4d08c98c2983fd2042a86c89e0bd6
abelov <be10ff@yandex.ru>
2018-03-23 18:04:09 +0300

    bugfix 1.0.1 found Picasso problem

--- schemecomponent/src/main/java/ru/abelov/schemeTimeComponent/scheme/ControlTable.java
+++ schemecomponent/src/main/java/ru/abelov/schemeTimeComponent/scheme/ControlTable.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
@@ -18,7 +19,7 @@ import ru.abelov.schemeTimeComponent.entity.ITable;
 
 public class ControlTable extends FrameLayout {
 
-    android.support.v7.widget.AppCompatImageView ivTable;
+    ImageView ivTable;
     View flControl;
 
     ITable tableInfo;
@@ -30,8 +31,8 @@ public class ControlTable extends FrameLayout {
     public ControlTable(final ViewGroup container, final OnTableSelectListener listener, final ITable tableInfo, TableStatusData data) {
         super(container.getContext());
         View v = ((LayoutInflater) container.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.table_view, this);
-        ivTable =/* (android.support.v7.widget.AppCompatImageView)*/v.findViewById(R.id.ivTable);
-        flControl =/* (android.support.v7.widget.AppCompatImageView)*/v.findViewById(R.id.flControl);
+        ivTable = (ImageView)v.findViewById(R.id.ivTable);
+        flControl = v.findViewById(R.id.flControl);
         this.mData = data;
 
         this.tableInfo = tableInfo;
@@ -67,102 +68,6 @@ public class ControlTable extends FrameLayout {
         setLayoutParams(params);
     }
 
-//    public void _onTimeChanged() {
-//
-//        if(mData.isMyBookingOrder(tableInfo)){
-//
-//            switch(tableInfo.getCapacity()){
-//                case 0:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren);
-//                    break;
-//                case 2:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren_2);
-//                    break;
-//                case 3:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren_3);
-//                    break;
-//                case 4:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren_4);
-//                    break;
-//                case 5:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren_5);
-//                    break;
-//                case 6:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren_6);
-//                    break;
-//                case 7:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren_8);
-//                    break;
-//                case 8:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren_8);
-//                    break;
-//                default:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_gren);
-//            }
-////            ivTable.setImageResource(R.drawable.ic_table_8_green);
-//        } else if(mData.isBuzy(tableInfo)){
-//            switch(tableInfo.getCapacity()){
-//                case 0:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red);
-//                    break;
-//                case 2:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red_2);
-//                    break;
-//                case 3:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red_3);
-//                    break;
-//                case 4:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red_4);
-//                    break;
-//                case 5:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red_5);
-//                    break;
-//                case 6:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red_6);
-//                    break;
-//                case 7:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red_8);
-//                    break;
-//                case 8:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red_8);
-//                    break;
-//                default:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_red);
-//            }
-////            ivTable.setImageResource(R.drawable.ic_table_8_red);
-//        } else {
-//            switch(tableInfo.getCapacity()){
-//                case 0:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_white);
-//                    break;
-//                case 2:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_2);
-//                    break;
-//                case 3:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_3);
-//                    break;
-//                case 4:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_4);
-//                    break;
-//                case 5:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_6);
-//                    break;
-//                case 6:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_6);
-//                    break;
-//                case 7:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_8);
-//                    break;
-//                case 8:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_8);
-//                    break;
-//                default:
-//                    ivTable.setImageResource(R.drawable.ic_table_for_white);
-//            }
-//        }
-//
-//    }
-//    String url = "http://img.bakapp.ru/image/table/file?categoryId=9&type=table&orientation=ordinal&personsCount=4";
 public void onTimeChanged() {
 
     String url = tableInfo.getImageUrl();
